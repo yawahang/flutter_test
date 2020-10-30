d384d1d26831e96052982beb91392d3765a8d446
Yawa Rai <yawa.rai@aqore.com>
2020-10-30 20:26:52 +0545

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,33 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+const _appHeaderTextStyle = TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              );
+
+final _bodyTextStyle = TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[800],
+                fontFamily: 'IndieFlower',
+              );
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
- 
+
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'My App',
+      theme: ThemeData( // overall app theme
+        primaryColor: Colors.red,
+      ),
       home: RandomWords(),
     );
   }
@@ -21,9 +39,9 @@ class MyApp extends StatelessWidget {
 class _RandomWordsState extends State<RandomWords> {
   
   final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
   
-   Widget _buildSuggestions() {
+  Widget _buildSuggestions() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
@@ -36,15 +54,79 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10));  
           }
           return _buildRow(_suggestions[index]);
-        });
+    });
   }
 
-  Widget _buildRow(WordPair pair) {     
+  Widget _buildRow(WordPair pair) {    
+
+    final alreadySaved = _saved.contains(pair); 
     return ListTile(
+      leading: Image(
+          image: AssetImage('assets/images/icon-blue-bg.png'),
+          fit: BoxFit.cover,
+        ),
+      //  Image.asset(
+      //   'assets/images/icon-blue-bg.png',
+      //   fit: BoxFit.cover,
+      // ),
+      // Image(
+      //   image: NetworkImage('https://cdn.auth0.com/blog/illustrations/flutter.png'),
+      //   fit: BoxFit.cover,
+      // ),
       title: Text(
         pair.asPascalCase,
-        style: _biggerFont,
-      )
+        style: _bodyTextStyle,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ), 
+      onTap: () {
+      setState(() {
+        if (alreadySaved) {
+
+          _saved.remove(pair);
+        } else { 
+
+          _saved.add(pair); 
+        } 
+      });
+    },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>( 
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _bodyTextStyle,
+                  ),
+                );
+              },
+            );
+
+            final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+          return Scaffold(
+              appBar: AppBar(
+              title: Text(
+                'Saved Suggestions',
+                style: _appHeaderTextStyle
+              ),
+              centerTitle: true,
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
@@ -52,9 +134,28 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text(
+            'App Title',
+            style: _appHeaderTextStyle,
+          ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+              print('button clicked!');
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.red
+        ),
     );
   }
 }
